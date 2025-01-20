d8a1bb912c88b1e6ba7abc540116b0126da05550
AAS696969 <138122939+AAS696969@users.noreply.github.com>
2025-01-20 19:49:51 +0300

    Add files via upload

---
+++ main5.py
@@ -0,0 +1,125 @@
+from selenium import webdriver
+from time import sleep
+
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+import datetime
+
+file = open("log1.txt", "w")
+
+#driver = webdriver.Chrome()
+
+option = webdriver.ChromeOptions()
+# option.add_experimental_option('detach', True)
+option.add_argument("--headless")
+driver = webdriver.Chrome(options=option)
+
+
+# end of setup
+
+# SC functions............
+def set_up():
+    driver.get("http://www.saucedemo.com/")
+    driver.maximize_window()
+
+# End of SC functions............
+
+# Tests........................
+def login():
+    user_name = driver.find_element(By.XPATH,  '//input[@id="user-name"]')
+    login1 = "standard_user"
+    user_name.send_keys(login1)
+    file.write("Success write login\n")
+
+    password = driver.find_element(By.XPATH,  '//input[@id="password"]')
+    user_pass = "secret_sauce"
+    password.send_keys(user_pass)
+    file.write("Success write password\n")
+
+    login_button= driver.find_element(By.XPATH, '//input[@id="login-button"]' )
+    login_button.click()
+    file.write("Success click login\n")
+
+def login_with_enter():
+    user_name = driver.find_element(By.XPATH,  '//input[@id="user-name"]')
+    login1 = "standard_user"
+    user_name.send_keys(login1)
+    file.write("Success write login\n")
+
+    password = driver.find_element(By.XPATH,  '//input[@id="password"]')
+    user_pass = "secret_sauce"
+    password.send_keys(user_pass)
+    file.write("Success write password\n")
+
+    user_pass.send_keys(Keys.ENTER)
+    file.write("Success Enter login\n")
+
+def fake_login():
+    user_name = driver.find_element(By.XPATH,  '//input[@id="user-name"]')
+    login1 = "standard_user"
+    user_name.send_keys(login1)
+    file.write("Success write login\n")
+
+    password = driver.find_element(By.XPATH,  '//input[@id="password"]')
+    user_pass = "secret_sauce1"
+    password.send_keys(user_pass)
+    file.write("Success fake password\n")
+
+    login_button= driver.find_element(By.XPATH, '//input[@id="login-button"]' )
+    login_button.click()
+    file.write("Success click login\n")
+
+def test_login_redirect():
+    correct_url = "https://www.saucedemo.com/inventory.html"
+    get_url = driver.current_url
+
+    assert correct_url == get_url, "test_login_redirect is Failed"
+    file.write("test_login_redirect is OK\n")
+
+def refresh_page():
+    driver.refresh()
+
+def test_context_after_login_is_correct():
+    correct_text = "Products"
+    current_text = driver.find_element(By.XPATH,  '//*[@id="header_container"]/div[2]/span')
+
+    driver.save_screenshot(f"screenshots1\\screenshot_test_context_after_login_is_correct_"
+                           f"{datetime.datetime.now().strftime("%H%M.%d%m%Y")}.png")
+    assert correct_text == current_text.text, "test_context_after_login_is_correct is Failed"
+    file.write("test_context_after_login_is_correct is OK\n")
+
+def test_fake_login_label():
+    correct_text = "Epic sadface: Username and password do not match any user in this service"
+    current_text = driver.find_element(By.XPATH,  '//*[@id="login_button_container"]/div/form/div[3]/h3')
+
+    assert correct_text == current_text.text,"test_fake_login_label is Failed"
+    file.write("test_fake_login_label is OK\n")
+
+# main
+def sc_real_login():
+    set_up()
+    login()
+
+    test_login_redirect()
+    test_context_after_login_is_correct()
+
+def sc_fake_login():
+    set_up()
+    fake_login()
+
+    test_fake_login_label()
+
+def sc_real_login_with_ENTER():
+    set_up()
+    login_with_enter()
+
+    test_login_redirect()
+    test_context_after_login_is_correct()
+#.................
+
+sc_real_login()
+#sc_fake_login()
+
+
+file.close()
+driver.quit()
